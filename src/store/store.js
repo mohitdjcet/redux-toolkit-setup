9b2210b9a1a03e34b6843f259f571d1400d243fc
Mohit Kumar <mks.djcet@gmail.com>
2025-07-24 21:46:12 +0530

    Update loggerMiddleware

--- src/store/store.js
+++ src/store/store.js
@@ -2,9 +2,19 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "../features/counter/counterSlice";
 import authReducer from "../features/auth/authSlice";
 
+const loggerMiddleware = storeAPI => next => action => {
+  console.log('Dispatching action:', action);
+  const result = next(action);
+  console.log('Next state:', storeAPI.getState());
+  return result;
+};
+
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     auth: authReducer
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(loggerMiddleware),
+  // devTools: false,
 });
